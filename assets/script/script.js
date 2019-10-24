c043c78cd1609cc289b9e25570c2e23742e55830
schevet <scheveuni@gmail.com>
2019-10-23 20:38:09 -0500

    added loop for space characters

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,37 +1,51 @@
 $(document).ready(function () {
 
     // $(".submit").on("click", function (event) {
-        // event.preventDefault()
+    // event.preventDefault()
 
-        var initialBreweryURL = "https://api.openbrewerydb.org/breweries?by_city={cityname}&by_state={state}";
-        var city = "minneapolis";
-        var state = "Minnesota";
+    var initialBreweryURL = "https://api.openbrewerydb.org/breweries?by_city={cityname}&by_state={state}";
+    var city = "Des Moines";
+    var state = "Iowa";
 
-        var cityBreweryURL = initialBreweryURL.replace("{cityname}", city);
-        var stateBreweryURL = cityBreweryURL.replace("{state}",state);
-        
-        var breweryURL = stateBreweryURL;
+    for(i=0; i < city.length; i++){
+        if (city.charAt[i] === "d") {
+            city.replace( "d", "%20");
+        }
+    }
+    console.log(city)
 
-        console.log(breweryURL);
+    for(i=0; i < state.length; i++){
+        if (state.charAt[i] === " ") {
+            state.replace(" ", "%20");
+        }
+    }
+    console.log(state)
 
-        // ajax for zomato
-        $.ajax({
-            url: "https://developers.zomato.com/api/v2.1/search?entity_id=minneapolis&entity_type=city&q=burgers",
-            dataType: 'json',
-            async: true,
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('user-key',
-                    'd88928eafacfec3391be0d039bd9daa4');
-            },
-        }).then(function (response) {
-            console.log(response);
-        });
-        
-// ajax for brewery
+    var cityBreweryURL = initialBreweryURL.replace("{cityname}", city);
+    var stateBreweryURL = cityBreweryURL.replace("{state}", state);
+
+    var breweryURL = stateBreweryURL;
+
+    // console.log(breweryURL);
+
+    // ajax for zomato
+    $.ajax({
+        url: "https://developers.zomato.com/api/v2.1/search?entity_id=minneapolis&entity_type=city&q=burgers",
+        dataType: 'json',
+        async: true,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('user-key',
+                'd88928eafacfec3391be0d039bd9daa4');
+        },
+    }).then(function (response) {
+        // console.log(response);
+    });
+
+    // ajax for brewery
     $.ajax({
         url: breweryURL,
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
         // var theResult = response;
         // console.log(theResult);
     });
