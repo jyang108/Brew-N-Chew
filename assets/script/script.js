c58023b23cc8f1653a8d315897e07f72c548a848
schevet <scheveuni@gmail.com>
2019-10-28 20:13:12 -0500

    updates on brewery results

--- assets/script/script.js
+++ assets/script/script.js
@@ -34,11 +34,17 @@ $(document).ready(function () {
                 var breweryName = breweryURL[i].name;
                 var breweryType = breweryURL[i].brewery_type;
                 var brewerySite = breweryURL[i].website_url;
-                console.log(breweryName + breweryType + brewerySite);
-                
-                $("#drinkResults").append("<h2>" + breweryName);
-                $("#drinkResults").append("<h3>" + breweryType);
-                $("#drinkResults").append("<h3>" + brewerySite);
+
+                var drinkResultCard = $("#drinkResults").append("<div class ='card'>");
+                var drinkResultCardBody = drinkResultCard.append("<div class='card-body'");
+
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
             };
         });
 
