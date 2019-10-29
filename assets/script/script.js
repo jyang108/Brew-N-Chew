93accbc6a26b19e243c5cd94af1f651df4dca2e6
schevet <scheveuni@gmail.com>
2019-10-28 20:16:49 -0500

    updates

--- assets/script/script.js
+++ assets/script/script.js
@@ -38,13 +38,11 @@ $(document).ready(function () {
                 var drinkResultCard = $("#drinkResults").append("<div class ='card'>");
                 var drinkResultCardBody = drinkResultCard.append("<div class='card-body'");
 
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
+    // <h5 class="card-title">Card title</h5>
+    // <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    // <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    // <a href="#" class="card-link">Card link</a>
+    // <a href="#" class="card-link">Another link</a>
             };
         });
 
